1781cbd98f0e2c308974e481a9d03c53aaa1c776
Daniil <jeiromkrizhanovskiyd@gmail.com>
2025-05-10 14:12:57 +0500

    Installing libraries: psycopg2, Pillow, dotenv. A PostgreSQL database has been created and connected, the users application has been created, a model of the same name has been created, and migrations have been sent.

---
+++ users/models.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Email",
+    )
+    avatar = models.ImageField(
+        upload_to="users/",
+        blank=True,
+        null=True,
+        verbose_name="Аватар",
+    )
+    phone = models.CharField(
+        max_length=11,
+        blank=True,
+        null=True,
+        verbose_name="Номер телефона",
+    )
+    city = models.CharField(
+        max_length=50, blank=True, null=True, verbose_name="Город"
+    )
+    token = models.CharField(
+        max_length=150, verbose_name="Токен", blank=True, null=True
+    )
+    is_active = models.BooleanField(blank=True, null=True)
+    tg_id = models.CharField(max_length=255, blank=True, null=True, verbose_name="Telegram ID")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
