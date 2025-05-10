89916736ee1e8c05820f7df230f161302c9dfaa6
Daniil <jeiromkrizhanovskiyd@gmail.com>
2025-05-10 15:57:04 +0500

    Adding basic functionality and classes to the application.

---
+++ mvita_main/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MvitaMainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mvita_main'
---
+++ mvita_main/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Doctors(models.Model):
+    pass
+
+
+class Services(models.Model):
+    pass
+
+
+class Reviews(models.Model):
+    pass
+
+
+class Information(models.Model):
+    pass
+
