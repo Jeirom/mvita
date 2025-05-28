4b419746f63eb5c699a14cfccf624d9eaf622155
Daniil <jeiromkrizhanovskiyd@gmail.com>
2025-05-28 16:15:11 +0500

    Фиксы главной страницы doctors_form.html, добавлены ссылки в выпадающем меню. Добавлены модели в админку

--- mvita_main/admin.py
+++ mvita_main/admin.py
@@ -1,31 +1,8 @@
-from mvita_main.models import Doctors, Information
+from mvita_main.models import Doctors, Information, DiagnosticResults, Record, Services
 
 from django.contrib import admin
 
 
-# # Направление в медицине
-# @admin.register(Reviews)
-# class ReviewsAdmin(admin.ModelAdmin):
-#     list_display = ("id", "name", "description",)
-#     list_filter = ("name",)
-#     search_fields = ("name",)
-
-
-# # Доктора
-# @admin.register(Doctors)
-# class DoctorsAdmin(admin.ModelAdmin):
-#     list_display = ("id", "first_name", "last_name", "patronymic", "medical_direction",
-#                     "avatar", "specialization", "experience", "user", )
-#     list_filter = ("last_name", "specialization", "reviews", "medical_direction",)
-#     search_fields = ("experience", "id", "medical_direction", )
-
-
-# # Услуги
-# @admin.register(Services)
-# class ServicesAdmin(admin.ModelAdmin):
-#     list_display = ("id", "name", "medical_direction", "description", "price", "user",)
-#     list_filter = ("name", "description",)
-#     search_fields = ("name", "description",)
 
 
 # Информация
@@ -42,8 +19,6 @@ class InformationAdmin(admin.ModelAdmin):
         "info7",
         "image_main",
     )
-    # list_filter = ("address",)
-    # search_fields = ("address",)
 
 
 # Доктор
@@ -56,42 +31,21 @@ class DoctorsAdmin(admin.ModelAdmin):
         "image",
         "specialization",
     )
-    # list_filter = ("address",)
-    # search_fields = ("address",)
 
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('patient_name', 'doctor', 'appointment_date', 'address')
+    search_fields = ('patient_name',)
+    list_filter = ('address', 'doctor')
 
-#
-# # Ценности
-# @admin.register(CompanyValues)
-# class  CompanyValuesAdmin(admin.ModelAdmin):
-#     list_display = ("name", "description", )
-#
-#
-# # Запись на приём
-# @admin.register(Appointment)
-# class AppointmentAdmin(admin.ModelAdmin):
-#     list_display = ("id", "status", "address", "doctor", "appointment_date", "services", "is_active", "user",)
-#     list_filter = ("appointment_date", "user", "services")
-#     search_fields = ("appointment_date", "user", "services")
-#
-#
-# # Результаты диагностики
-# @admin.register(DiagnosticResults)
-# class DiagnosticResultsAdmin(admin.ModelAdmin):
-#     list_display = ("id", "appointment", "recommendations", "user", "general_comments",)
-#     list_filter = ("appointment",)
-#     search_fields = ("appointment",)
-#
-#
-# # Медицинские тесты. Результаты
-# @admin.register(TestResult)
-# class DiagnosticResultsAdmin(admin.ModelAdmin):
-#     list_display = ("id", "diagnostic_result", "name", "value", "norm", "comment",)
-#     list_filter = ("name",)
-#     search_fields = ("name",)
-#
-#
-# # Обратная связь
-# @admin.register(Feedback)
-# class  CompanyValuesAdmin(admin.ModelAdmin):
-#     list_display = ("subject", "feedback", "user", "created_at",)
+@admin.register(DiagnosticResults)
+class DiagnosticResultsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'record', 'results')
+    search_fields = ('results',)
+    list_filter = ('user',)
+
+@admin.register(Services)
+class ServicesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    search_fields = ('name',)
+    list_filter = ('name',)
